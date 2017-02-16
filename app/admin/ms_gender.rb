b3b990e5b4de9396d3098fc5718675441f5e71b7
Prelang Builder <builder@prelang.com>
2017-02-16 04:59:01 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'MsGender' to allow for create, update

--- app/admin/ms_gender.rb
+++ app/admin/ms_gender.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register MsGender do
+  permit_params :name, :is_active
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
